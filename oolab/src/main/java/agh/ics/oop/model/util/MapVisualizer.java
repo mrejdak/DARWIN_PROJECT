7b8639ba160825c4eda36fcbcfab78096ab9b5d6
mrejdak <mrejdak@student.agh.edu.pl>
2025-04-05 23:36:09 +0200

    removed console display

--- oolab/src/main/java/agh/ics/oop/model/util/MapVisualizer.java
+++ oolab/src/main/java/agh/ics/oop/model/util/MapVisualizer.java
@@ -2,7 +2,7 @@ package agh.ics.oop.model.util;
 
 import agh.ics.oop.model.Vector2d;
 import agh.ics.oop.model.WorldMap;
-
+//TODO: remove
 /**
  * The map visualizer converts the {@link WorldMap} map into a string
  * representation.
